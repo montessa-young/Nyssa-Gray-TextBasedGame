88b1beb67101ad29c7d5bc8630c87d13fc12215b
Derek <10dpadilla@nyssasd.org>
2025-12-11 22:06:55 +0000

    Mrs. Young adding missing files

---
+++ armor.py
@@ -0,0 +1,12 @@
+
+def armor_price():
+    print("                      ~~~~~~ARMOR~~~~~~")
+    print("1 Titanium- Makes you shoot and drive slower, add 100 health")
+    print("2 leather- Shoot and drive normaly, add 50 health")
+    print("3 Paper bag- Shoot and drive boosted by 1.5, add 25 health")
+    print("4 No armor- All normal, no added health")
+    titanium-750
+    leather-500
+    paper_bag-250
+    no_armor-0
+
